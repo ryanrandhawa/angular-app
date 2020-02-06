cf8c69489c6f819c26386414e2a7fea411d0dc99
Ryan Randhawa <ryan.s.randhawa@avanade.com>
2020-02-05 20:45:50 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TestcompComponent } from './testcomp/testcomp.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestcompComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
